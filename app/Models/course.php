98fc038c83282e737015f43739e4bc82d4dea6a5
Shayekh Mohiuddin Ahmed Navid <shayekhnavid@gmail.com>
2021-02-22 02:12:47 +0600

    Course Model Attribute updated

--- app/Models/course.php
+++ app/Models/course.php
@@ -12,7 +12,11 @@ class course extends Model
     protected $guarded =[
         'course_id','courseexperts_id',
         'accepted_appointment_id,','requested_appointment_id,',
-        'course_code','university_name',
+        'course_code1','university_name1','course_code2',
+        'university_name2','course_code3','university_name3',
+        'course_code4','university_name4','course_code5','university_name5',
+        'course_code6','university_name6','course_code7','university_name7',
+        'course_code8','university_name8',
     ];
 
 
