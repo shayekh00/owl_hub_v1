2ef13e4fe161988b5878595dc2c43ab6a69ce50f
Shayekh Mohiuddin Ahmed Navid <shayekhnavid@gmail.com>
2021-03-11 18:10:37 +0600

    Student Request Implementation

---
+++ app/Http/Controllers/StudentRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use DB;
+
+class StudentRequest extends Controller
+{
+
+    function index()
+    {
+    
+    $courseexpert_id  =Auth::guard('courseexpert')->user()->courseexpert_id;
+    // $courseexpert_id=1;    
+    
+    $appointment_images_data = DB::select("SELECT DISTINCT C.course_code1, B.problem_text
+    FROM accepted_appointments AS A , appointment_images AS B, courses AS C
+    WHERE A.accepted_appointment_id = B.accepted_appointment_id
+    AND A.course_id = C.course_id
+    AND A.courseexpert_id = $courseexpert_id");
+
+    //dd($appointment_images_data);
+
+    return view('courseexperts.studentrequest',
+    ['appointment_images_data' => $appointment_images_data    ]);
+
+    }
+
+    function action(Request $request)
+    {
+
+    }
+}
