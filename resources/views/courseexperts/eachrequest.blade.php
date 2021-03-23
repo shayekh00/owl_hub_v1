3990749822c8cd6035cd00dfa6681a1b3d1a1f22
UmmeRitu <umme.ritu@northsouth.edum>
2021-03-23 23:56:25 +0600

    Each Request Blade File

--- resources/views/courseexperts/eachrequest.blade.php
+++ resources/views/courseexperts/eachrequest.blade.php
@@ -1,41 +1,53 @@
 @extends('html_skeleton')
 
 @section('css_file')
-    <link href="{{ asset('css/landing.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/course_expert_courses.css') }}" rel="stylesheet">
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 @endsection
+
+
+
 @section('content')
+
 
 @extends('courseexperts.courseexpertnavbar')
 
-<!-- <div class="container">
-    <h1 class=" owl_hub_green" >Welcome To Owl Hub</h1>
+
+<div class="container">
+  <!--  <h1 class=" owl_hub_green" >Welcome To Owl Hub</h1>---->
 </div>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script> -->
 <div class="container">
     <div class="row justify-content-center">
       <div class="col-sm-4">  
           <div class="card"> 
-            @foreach($accepted_appointments as $key => $data)
+             
                  <div class="card-body">
                     <div class="text-success">
-                    <h2 class="text-center">{{$data->course_id}}</h2>
-                    <h2 class="text-center">{{$data->appointment_timing}}</h2>
-            @endforeach
-
-            @foreach($appointment_images as $key => $variable)
-            <h2 class="text-center">{{$variable->problem_text}}</h2>
-            <h2 class="text-center">Resources                                     
-                    <a href="#">
-                    <span class="text-success">{{$variable->filename}}</span>
-                    </a> 
-                    </p>
-                    <button type="button" class="btn btn-success">Accept</button> 
-            @endforeach
-                  </div>
-                    @if (session('status'))
+                    <h4>Course ID</h4>
+                    <h4>Course Timing</h4>
+                    <h4>Problem Text</h4>
+                    <h4>Resources                                     
+                            <a href="#">
+                            <span class="glyphicon glyphicon-picture" style="color:green;"></span>
+                            </a> 
+                            <a href="#">
+                            <span class="glyphicon glyphicon-picture" style="color:green;"></span>
+                            </a>
+                            <a href="#">
+                            <span class="glyphicon glyphicon-picture" style="color:green;"></span>
+                            </a>
+                            </p>
+                            <div class="text-center">
+                            <button type="button" class="btn btn-success">Accept</button> 
+                            <button type="button" class="btn btn-danger" >Reject</button> 
+                            </div>     
+                 </div>
+                   @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
