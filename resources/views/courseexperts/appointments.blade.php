c632b815e48b325693a1109ffd9b96c959626a7d
asifuzzamanchy <61204398+asifuzzamanchy@users.noreply.github.com>
2021-04-26 03:00:49 +0600

    sidebar fixed

--- resources/views/courseexperts/appointments.blade.php
+++ resources/views/courseexperts/appointments.blade.php
@@ -24,16 +24,7 @@
 
 
 
-<div class="sidenav">
-    <a href="/appointments" style="color: white;font-weight:Bold ; font-size:20px;">My Appointments</a>
-    <a href="/update_expert_profile" style="color: white;font-weight:Bold ; font-size:20px;">Edit Profile</a>
-    <a href="/skypeLink" style="color: white;font-weight:Bold ; font-size:20px;">Add links</a>
-    <a href="/view_timing" style="color: white;font-weight:Bold ; font-size:20px;">View Timing</a>
-    <!-- <a href="/experttiming" style="color: white;font-weight:Bold ; font-size:20px;">Update your Timing</a> -->
-    <a href="/view_courses" style="color: white;font-weight:Bold ; font-size:20px;">View Courses</a>
-    <!-- <a href="/expertcourses" style="color: white;font-weight:Bold ; font-size:20px;">Update your Courses</a> -->
-    <a href="/update_expert_course" style="color: white;font-weight:Bold ; font-size:20px;">Delete Courses</a>
-</div>
+@extends('courseexperts.courseexpertsidebar')
 
 @if( count($accepted_appointments)== 0 )
         <div class="alert alert-primary" role="alert">
--- resources/views/courseexperts/expertProfile.blade.php
+++ resources/views/courseexperts/expertProfile.blade.php
@@ -16,17 +16,7 @@
                  <h1 class=" owl_hub_green" >My Profile</h1>
 </div>
 
-
-<div class="sidenav">
-    <a href="/appointments" style="color: white;font-weight:Bold ; font-size:20px;">My Appointments</a>
-    <a href="/update_expert_profile" style="color: white;font-weight:Bold ; font-size:20px;">Edit Profile</a>
-    <a href="/skypeLink" style="color: white;font-weight:Bold ; font-size:20px;">Add links</a>
-    <a href="/view_timing" style="color: white;font-weight:Bold ; font-size:20px;">View Timing</a>
-    <!-- <a href="/experttiming" style="color: white;font-weight:Bold ; font-size:20px;">Update your Timing</a> -->
-    <a href="/view_courses" style="color: white;font-weight:Bold ; font-size:20px;">View Courses</a>
-    <!-- <a href="/expertcourses" style="color: white;font-weight:Bold ; font-size:20px;">Update your Courses</a> -->
-    <a href="/update_expert_course" style="color: white;font-weight:Bold ; font-size:20px;">Delete Courses</a>
-</div>
+@extends('courseexperts.courseexpertsidebar')
 
 
 
