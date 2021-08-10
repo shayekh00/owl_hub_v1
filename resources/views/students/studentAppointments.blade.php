b1209806ca27dcecd3b40294383ad981183e5b3d
shayekh00 <shayekhnavid@gmail.com>
2021-08-10 13:02:33 +0600

    Side Bar changed for student

--- resources/views/students/studentAppointments.blade.php
+++ resources/views/students/studentAppointments.blade.php
@@ -2,45 +2,99 @@
 @extends('html_skeleton')
 
 @section('css_file')
-        <link href="{{ asset('css/expertProfile.css') }}" rel="stylesheet">
+        <!-- <link href="{{ asset('css/expertProfile.css') }}" rel="stylesheet"> -->
+        
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-@endsection
+        <link href="{{ asset('css/sidebar_new.css') }}" rel="stylesheet">
 
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+
+@endsection
 
 
 @section('content')
 
 @extends('students.studentsnavbar')
-<div class="center">
-                 <h1 class="owl_hub_green display-4 font-weight-bold" >My Appointments</h1>
-</div>
-
-<div class="sidenav">
-    <a href="/student_profile_update" style="color: white;font-weight:Bold ; font-size:20px;">Edit Profile</a>
-    <a href="/Appointments" style="color: white;font-weight:Bold ; font-size:20px;">Appointments</a>
-</div>
-
-@foreach ($accepted_appointments as  $key => $data )
-        
-        <div class="row">
-
-            <div class="column">
-
-                <div class="card">
 
 
-                    <br>
-                    <div class="container">
-                                <p > <b> Appointment ID: </b>  {{ $data->accepted_appointment_id }} </p>
-                                <p > <b> Course Name: </b>  {{ $data->course_code1 }} </p>
-                                <p > <b> Appointment Timing: </b>  {{ $data->appointment_timing }} </p>
-                                                      
+<div class="wrapper">
+        <!-- Sidebar  -->
+        <nav id="sidebar">
+            <!-- <div class="sidebar-header">
+                <h3>Bootstrap Sidebar</h3>
+            </div> -->
+
+            <ul class="list-unstyled components">
+                
+                <li>
+                    <a href="/student_profile_update" style="color: white;font-weight:Bold ; font-size:20px;">Edit Profile</a>
+                </li>
+                <li>
+                    <a href="/Appointments" style="color: white;font-weight:Bold ; font-size:20px;">Appointments</a>
+                </li>
+
+                <!-- <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+
+                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
+                    <ul class="collapse list-unstyled" id="pageSubmenu"> -->
+
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+
+    <div  id="content" class="center">
+        <div class="center">
+                    <h1 class="owl_hub_green display-4 font-weight-bold" >My Appointments</h1>
+        </div>
+
+        <div class="content center">
+                @foreach ($accepted_appointments as  $key => $data )
+                
+                <div class="row">
+
+                    <div class="column">
+
+                        <div class="card">
+
+
+                            <br>
+                            <div class="container">
+                                        <p > <b> Appointment ID: </b>  {{ $data->accepted_appointment_id }} </p>
+                                        <p > <b> Course Name: </b>  {{ $data->course_code1 }} </p>
+                                        <p > <b> Appointment Timing: </b>  {{ $data->appointment_timing }} </p>
+                                                            
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+                <br>
+                @endforeach
+                    <br>
         </div>
-        <br>
-@endforeach
+        
+    </div>
+</div>
+
+
+
+<!-- jQuery CDN - Slim version (=without AJAX) -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<!-- Popper.JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<!-- Bootstrap JS -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+<script type="text/javascript">
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+});
 
 
 @endsection
--- resources/views/students/studentprofile.blade.php
+++ resources/views/students/studentprofile.blade.php
@@ -2,65 +2,102 @@
 @extends('html_skeleton')
 
 @section('css_file')
-        <link href="{{ asset('css/expertProfile.css') }}" rel="stylesheet">
+        <!-- <link href="{{ asset('css/expertProfile.css') }}" rel="stylesheet"> -->
+        
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-@endsection
+        <link href="{{ asset('css/sidebar_new.css') }}" rel="stylesheet">
 
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+
+@endsection
 
 
 @section('content')
 
 @extends('students.studentsnavbar')
-<div class="center">
-                 <h1 class="owl_hub_green display-4 font-weight-bold" >My Profile</h1>
-</div>
-
-<div class="sidenav">
-    <a href="/student_profile_update" style="color: white;font-weight:Bold ; font-size:20px;">Edit Profile</a>
-    <a href="/Appointments" style="color: white;font-weight:Bold ; font-size:20px;">Appointments</a>
-</div>
 
 
-<div class="content">
-<div class="row">
+<div class="wrapper">
+        <!-- Sidebar  -->
+        <nav id="sidebar">
+            <!-- <div class="sidebar-header">
+                <h3>Bootstrap Sidebar</h3>
+            </div> -->
 
-<div class="column">
-
-    <div class="card">
-    <!-- <h4>My Profile</h4> -->
-        
-
-    
-
-        <br>
-        <form action="profile-update"  method="POST">
-                {{ csrf_field() }}
-            <div class="container">
-                        
-                            <p > <b> Name: </b>  {{Auth::guard('student')->user()->name}} </p>
-
-                            <p > <b> Phone: </b>  {{Auth::guard('student')->user()->phone}} </p>
-
-                            <p > <b> Email: </b>  {{Auth::guard('student')->user()->email}} </p>
-
-
-                       
+            <ul class="list-unstyled components">
                 
-                       
-                        <!-- <p></p> -->
-            </div>
-            @if (session('status'))
-                <div class="alert alert-success" padding ="1">
-                    {{ session('status') }}
+                <li>
+                    <a href="/student_profile_update" style="color: white;font-weight:Bold ; font-size:20px;">Edit Profile</a>
+                </li>
+                <li>
+                    <a href="/Appointments" style="color: white;font-weight:Bold ; font-size:20px;">Appointments</a>
+                </li>
+
+                <!-- <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+
+                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
+                    <ul class="collapse list-unstyled" id="pageSubmenu"> -->
+
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+
+    <div  id="content" class="center">
+        <div class="center">
+                    <h1 class="owl_hub_green display-4 font-weight-bold" >My Profile</h1>
+        </div>
+
+        <div class="content center">
+            <div class="row">
+
+                <div class="column">
+                    <div class="card">
+                        <br>
+                        <form action="profile-update"  method="POST">
+                                {{ csrf_field() }}
+                            <div class="container">
+                                        
+                                            <p > <b> Name: </b>  {{Auth::guard('student')->user()->name}} </p>
+
+                                            <p > <b> Phone: </b>  {{Auth::guard('student')->user()->phone}} </p>
+
+                                            <p > <b> Email: </b>  {{Auth::guard('student')->user()->email}} </p>
+                            </div>
+                            @if (session('status'))
+                                <div class="alert alert-success" padding ="1">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+                        </form>
+                    </div>
                 </div>
-            @endif
-        </form>
+            </div>
+            <br>
+        </div>
+        
     </div>
 </div>
-</div>
-<br>
 
-</div>
+
+
+<!-- jQuery CDN - Slim version (=without AJAX) -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<!-- Popper.JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<!-- Bootstrap JS -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+<script type="text/javascript">
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+});
 
 
 @endsection
